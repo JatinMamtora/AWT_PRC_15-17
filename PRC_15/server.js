751124a1180f74d758b923c306cca76ed8e62320
JatinMamtora <115716610+JatinMamtora@users.noreply.github.com>
2023-10-07 18:14:41 +0530

    updated

--- PRC_15/server.js
+++ PRC_15/server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Secure Server!');
+  res.send('Hello');
 });
 
 const privateKey = fs.readFileSync('path/to/private-key.pem', 'utf8');
--- PRC_16/server.js
+++ PRC_16/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
+const User = require('./models/user.schema')
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,35 +12,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(session({
-  secret: 'your-secret-key',
+  secret: 'jatin-mamtora',
   resave: false,
   saveUninitialized: true,
 }));
 
-const users = [];
 
-app.post('/register', (req, res) => {
-  const { username, password } = req.body;
 
-  if (users.some(user => user.username === username)) {
+app.post('/register',async(req, res) => {
+  const { usernm, paswrd } = req.body;
+
+  if (User.find({username: usernm})) {
     return res.status(400).json({ message: 'User already exists' });
   }
 
-  const newUser = { username, password };
-  users.push(newUser);
+  const newUser = await User.create({ username: usernm, password: paswrd });
 
   res.status(201).json({ message: 'Registration successful' });
 });
 
 app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-  
-    const user = users.find(user => user.username === username);
-    if (!user || user.password !== password) {
+  const { usernm, paswrd } = req.body;
+
+  const user = (User.find({username: usernm}));
+    if (!user || user.password !== paswrd) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
   
-    const token = jwt.sign({ username }, 'your-secret-key', { expiresIn: '1h' });
+    const token = jwt.sign({ usernm }, 'jatin-mamtora');
     res.cookie('token', token, { httpOnly: true });
     res.json({ message: 'Login successful' });
 });
